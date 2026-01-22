7f0d17b0e90426b96a10da486a0280e424cbf45c
Mahmoud Ahmed <mahmoud181712@gmail.com>
2026-01-22 17:04:47 +0300

    my updated portfolio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// let year = document.querySelector(".year");
+// year.innerText= new Date().getFullYear();  
+
+
+
+let menu = document.querySelector(".menu");
+let ul =document.querySelector(".links");
+let close = document.querySelector(".close");
+
+menu.addEventListener("click",function(){
+    ul.classList.remove("links");
+    ul.classList.toggle("active");
+    
+    
+})
+
+close.addEventListener("click", ()=>{
+    ul.classList.add("links");
+    ul.classList.remove("active");
+})
+
+
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-out',
+    once: true
+  });
+
